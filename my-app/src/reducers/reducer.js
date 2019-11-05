732e0260862538392f5687c6c93a982f1500a113
ddelfaus <ddelfaus@gmail.com>
2019-11-05 15:35:26 -0500

    step 1 and initialState

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,21 @@
+
+
+
+
+export const todoReducer = () => {
+
+  return   
+}
+
+
+
+export const initialSate = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+
+    }
+]
+
+
